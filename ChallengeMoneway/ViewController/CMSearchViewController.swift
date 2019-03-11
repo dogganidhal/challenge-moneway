c7b01c5c55f58a0d19e2fd07a3745c7838a0bd46
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-03-11 20:06:51 +0100

    First commit : Create Project and Podfile

---
+++ ChallengeMoneway/ViewController/CMSearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CMSearchViewController.swift
+//  ChallengeMoneway
+//
+//  Created by Nidhal DOGGA on 11/03/2019.
+//  Copyright © 2019 Nidhal DOGGA. All rights reserved.
+//
+
+import UIKit
+
+class CMSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
